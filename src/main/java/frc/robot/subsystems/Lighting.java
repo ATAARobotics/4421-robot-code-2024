83a2358f4b7676dcda1ee69d1da537e42115175e
Nate Greco <34966406+Nate-Greco@users.noreply.github.com>
2024-01-30 18:47:00 -0700

    idk

--- src/main/java/frc/robot/subsystems/Lighting.java
+++ src/main/java/frc/robot/subsystems/Lighting.java
@@ -20,13 +20,13 @@ public class Lighting extends SubsystemBase {
     CANdle ledController = new CANdle(21);
 
     double targetX = 8.0;
-    double targetY = 4.0;
-    double targetAngle = 0.0;
+    double targetY = 4.0; 
+    final int targetAngle = 270;
 
     Swerve s_Swerve;
     double xDifference;
     double yDifference;
-    double angleDifference;
+    int angleDifference;
 
     public Lighting(Swerve s_Swerve) {
         CANdleConfiguration configAll = new CANdleConfiguration();
@@ -38,7 +38,7 @@ public class Lighting extends SubsystemBase {
         ledController.configAllSettings(configAll, 100);
         ledController.setLEDs(200, 200, 200);
 
-        ledController.setLEDs(0, 0, 0);
+        ledController.setLEDs(0, 0, 0, 0, 0, 128);
         
         this.s_Swerve = s_Swerve;
         
@@ -50,17 +50,17 @@ public class Lighting extends SubsystemBase {
             double[] pose = NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose").getDoubleArray(new double[6]);
             double poseX = -pose[0] + 8.27;
             double poseY = -pose[1] + 4.105;
-            Rotation2d poseR = Rotation2d.fromDegrees(pose[5] - 180);
+            double poseR = s_Swerve.getYaw().getDegrees();
             double timeStamp = Timer.getFPGATimestamp() - (pose[6] / 1000.0);
             xDifference = targetX - poseX;
             yDifference = targetY - poseY;
         
             double distance = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
     
-            angleDifference = targetAngle - poseR.getDegrees();
-    
-            int distColor = 0;
-            int angleInt = 0;
+            angleDifference = targetAngle - (int) Math.round(poseR);
+            System.out.println(Math.round(poseR));
+            int distInt = 1;
+            int angleColor = 0;
             // if (Math.abs(angleDifference) < 90) {
             //     if (Math.abs(distance) < 2.0) {
             //         distColor = (int) Math.round(Math.abs(distance * 127));
@@ -74,17 +74,23 @@ public class Lighting extends SubsystemBase {
             // }
             // else {
             //     ledController.setLEDs(200, 200, 200, 0, 0, 65);
-            // }
+            // } 
 
-            if (distance < 4.0) {
-                distColor = (int) Math.round(Math.abs(distance * 63));
+            if (distance <= 1.0) {
+                distInt = (int) Math.round(Math.abs(distance * 88));
+                // System.out.println(distance);
             }
 
-            if (Math.abs(angleDifference) < 90) {
-                angleInt = (int) Math.round(Math.abs(angleDifference * 1.42));
+            angleColor = (int) Math.round(Math.abs(angleDifference * 0.7));
+            double limelightid = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(-1.0);
+             if(limelightid <= 16 && limelightid >= 1 && distance != 1.0 ){
+                ledController.setLEDs(0, 0, 0, 0, distInt + 40, 128 - (distInt + 40));    
+                ledController.setLEDs(angleColor, 255 - angleColor, 0, 10, 0, distInt + 40);
+
+             
             }
 
-            ledController.setLEDs(distColor, 255 - distColor, 0, 10, 0, angleInt + 64);
+            
         }
 
        
