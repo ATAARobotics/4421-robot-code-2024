f5e59b4e5a02c72a62169cd8cb2d596e92a62842
Ronald Li <li.ronald2008@gmail.com>
2024-09-24 19:19:52 -0600

    Add alliance selection to constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,6 +22,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -90,6 +91,12 @@ public class RobotContainer {
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() { 
+    // comp alliance selection
+    // Constants.Swerve.setAlliance(DriverStation.getAlliance().get());
+
+    // testing alliance selection
+    Constants.Swerve.setAlliance(Alliance.Red);
+
     m_Index = new Index();
     m_Intake = new Intake();
     
@@ -263,11 +270,12 @@ public class RobotContainer {
   }
 
   public Boolean getSide(){
-    var alliance = DriverStation.getAlliance();
-    if (alliance.isPresent()) {
-      return alliance.get() == DriverStation.Alliance.Red;
-    }
-    return false;
+    // var alliance = DriverStation.getAlliance();
+    // if (alliance.isPresent()) {
+    //   return alliance.get() == DriverStation.Alliance.Red;
+    // }
+    // return false;
+    return true;
   }
   // public Swerve getSwerve(){
   //   return s_Swerve;
